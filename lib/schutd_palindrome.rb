ce52ba6fd7fc6a35775c0d16f5a170a65af17e62
David Schutze <davidschutze@gmail.com>
2018-10-03 20:47:31 -0400

    Finished palindrome method

--- lib/schutd_palindrome.rb
+++ lib/schutd_palindrome.rb
@@ -1,5 +1,17 @@
 require "schutd_palindrome/version"
 
-module SchutdPalindrome
-  # Your code goes here...
+class String
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+def letters
+  self.chars.select { |c| c.match(/[a-z]/i) }.join
+end
+
+  private
+
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
